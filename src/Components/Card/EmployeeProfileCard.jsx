45e241d7a420ecbf0da46dabd5c74105066d987e
pabel1 <pabelahamed39@gmail.com>
2024-01-07 21:50:15 +0600

    all user get and show employee card

--- src/Components/Card/EmployeeProfileCard.jsx
+++ src/Components/Card/EmployeeProfileCard.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import dummy from "../../../assets/images/dummyprofile.webp";
+import dummy from "../../assets/images/dummyprofile.webp";
 const EmployeeProfileCard = ({ data }) => {
   const { photo, name, email, userStatus, gender, _id } = data || {};
   return (
     <div className="border bg-white px-5 py-3 rounded-xl hover:shadow hover:border-primaryColor transition duration-300">
-      {/* <div className="flex justify-between items-center">
-        <div className={`rounded text-xs font-bold py-1 px-3 
-        ${employeeStatus === 'Permanent' ? 'bg-emerald-100 text-emerald-600' : employeeStatus === 'Probation' ? 'bg-blue-100 text-blue-600' : 'bg-orange-100 text-orange-600 '}`}>
-          {employeeStatus}
+      <div className="flex justify-between items-center">
+        <div
+          className={`rounded text-xs font-bold py-1 px-3 
+        ${
+          userStatus === "Active"
+            ? "bg-emerald-100 text-emerald-600"
+            : "bg-orange-100 text-orange-600 "
+        }`}
+        >
+          {userStatus}
         </div>
         <div>
-          <AdminEmployeeProfileOptionsDropdown profileData={profileData} />
+          {/* <AdminEmployeeProfileOptionsDropdown profileData={profileData} /> */}
         </div>
-      </div> */}
+      </div>
       <div>
         <>
           <div className="flex justify-center items-center shadow-med">
             <div className="border-primaryColor border-2 p-1.5 -mb-8 rounded-full w-32 h-32 overflow-hidden bg-white">
               <img
                 className="w-full h-full rounded-full object-cover"
-                src={photo?.url ? photo?.url : ""}
+                src={photo?.url ? photo?.url : dummy}
                 alt=""
               />
             </div>
@@ -54,7 +60,7 @@ const EmployeeProfileCard = ({ data }) => {
             <div className="flex flex-col w-full gap-y-2 mt-4">
               <Link
                 to={`/employee/${_id}`}
-                className="text-center text-sm py-2 border border-primaryColor rounded-md font-medium bg-primaryColor hover:bg-transparent text-white hover:text-primaryColor transition duration-300"
+                className="text-center text-sm py-2 border border-primaryColor rounded-md font-medium bg-primaryColor hover:bg-transparent  hover:text-primaryColor transition duration-300"
               >
                 View Profile
               </Link>
